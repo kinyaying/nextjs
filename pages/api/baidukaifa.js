c224c4674ce86bf3cf11b477c2390e8efe7f3a16
kinyaying <kinyaying@gmail.com>
2021-02-04 12:27:16 +0800

    feat: baidukaifa

---
+++ pages/api/baidukaifa.js
@@ -0,0 +1,20 @@
+import { createProxyMiddleware } from 'http-proxy-middleware'
+
+// Create proxy instance outside of request handler function to avoid unnecessary re-creation
+const apiProxy = createProxyMiddleware({
+  target: 'https://kaifa.baidu.com/',
+  changeOrigin: true,
+  pathRewrite: { '^/api/baidukaifa': '/rest/v1/search' },
+  secure: false,
+})
+
+export default function (req, res) {
+  apiProxy(req, res, (result) => {
+    if (result instanceof Error) {
+      throw result
+    }
+    throw new Error(
+      `Request '${req.url}' is not proxied! We should never reach here!`
+    )
+  })
+}
